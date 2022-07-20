5f6587d24d71b419a4df6c3cb9e1e55cfca72302
Giren <giren@web.de>
2022-07-20 12:57:59 +0200

    mic rausgeschmissen

--- src/main.cpp
+++ src/main.cpp
@@ -10,74 +10,6 @@
 #include "band.h"
 #include "analyzerWiFi.h"
 
-#define SAMPLE_SIZE 128
-#define SAMPLE_FREQUENCY 16000
-#define BANDS 7
-
-ADS1015 microphone;
-
-double vReal[SAMPLE_SIZE];
-double vImag[SAMPLE_SIZE];
-double maxvalue = 0;
-arduinoFFT FFT = arduinoFFT();
-
-int bands[] = {0, 0, 0, 0, 0, 0, 0};
-int bands_normalized[] = {0, 0, 0, 0, 0, 0, 0};
-
-bool bMicInitialized;
-
-int get_frequency(int i)
-{
-  if (i < 2)
-  {
-    return 0;
-  }
-  return (i - 2) * (SAMPLE_FREQUENCY * 2 / SAMPLE_SIZE);
-}
-
-void createBands(int i, int amplitude)
-{
-  int frequency = get_frequency(i);
-  uint8_t band = 0;
-
-  if (frequency <= 125)
-  {
-    band = 0;
-  }
-  else if (frequency <= 250)
-  {
-    band = 1;
-  }
-  else if (frequency <= 500)
-  {
-    band = 2;
-  }
-  else if (frequency <= 1000)
-  {
-    band = 3;
-  }
-  else if (frequency <= 2000)
-  {
-    band = 4;
-  }
-  else if (frequency <= 4000)
-  {
-    band = 5;
-  }
-  else if (frequency <= 8000)
-  {
-    band = 6;
-  }
-  else
-  {
-    band = 7;
-  }
-
-  const int max_amplitude = 2048;
-  amplitude = std::min(amplitude, max_amplitude);
-  bands[band] = std::max(amplitude, (int)band);
-}
-
 // Declare our NeoPixel strip object:
 static Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // Argument 1 = Number of pixels in NeoPixel strip
@@ -93,17 +25,6 @@ static Analyzer analyzer(&strip);
 void setup()
 {
   Serial.begin(115200);
-  Wire.begin(-1, -1, 1000000UL);
-
-  bMicInitialized = microphone.begin();
-  if (bMicInitialized)
-  {
-    Serial.println("Microphone found, I2C connected");
-  }
-  else
-  {
-    Serial.println("No device found.");
-  }
 
   setupWifi();
 
@@ -126,42 +47,7 @@ void setup()
 
 void loop()
 {
-  if (!bMicInitialized)
-  {
-    Serial.println("Mic not initialized...");
-    delay(1000);
-  }
-
-  for (int i = 0; i < SAMPLE_SIZE; i++)
-  {
-    vReal[i] = (double)microphone.getSingleEnded(2);
-    vImag[i] = 0.0;
-  }
-
-  FFT.Windowing(vReal, SAMPLE_SIZE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(vReal, vImag, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.ComplexToMagnitude(vReal, vImag, SAMPLE_SIZE);
-  double x = FFT.MajorPeak(vReal, SAMPLE_SIZE, SAMPLE_FREQUENCY);
-
-  int k = 0;
-  memset(bands, 0, sizeof(int) * 7);
-  std::stringstream result;
-  for (int i = 2; i < SAMPLE_SIZE / 2; i++)
-  {
-    createBands(i, vReal[i]);
-    // maxvalue = maxvalue < vReal[i] ? vReal[i] : maxvalue;
-    // result << std::setw(2) << k++ << ": ";
-    // result << std::setprecision(2) << std::fixed << std::setw(5) << vReal[i] << " ; ";
-  }
-
-  for (int i = 0; i < BANDS; i++)
-  {
-    bands_normalized[i] = bands[i] * 100 / 2048;
-    Serial.printf("%d : %3d | %4d\t", i + 1, bands_normalized[i], bands[i]);
-  }
-  Serial.println();
-
-  constexpr uint16_t REFRESH_RATE_MS = 200;
+  constexpr uint16_t REFRESH_RATE_MS = 1;
 
   static auto offset = millis();
   if ((millis() - offset) > REFRESH_RATE_MS)
