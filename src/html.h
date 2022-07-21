eee03e753f6cf7807b54080623adc3fd55f22177
Giren <giren@web.de>
2022-07-21 15:40:17 +0200

    button entfernt

--- src/html.h
+++ src/html.h
@@ -10,49 +10,15 @@ const char index_html[] PROGMEM = R"rawliteral(
     <head><meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="data:,">
 
-    <style>
-      .dropbtn {
-        background-color: #3498DB;
-        color: white;
-        padding: 16px;
-        font-size: 16px;
-        border: none;
-        cursor: pointer;
-      }
-      .dropbtn:hover, .dropbtn:focus {
-        background-color: #2980B9;
-      }
-      .dropdown {
-        position: relative;
-        display: inline-block;
-      }
-      .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f1f1f1;
-        min-width: 160px;
-        overflow: auto;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        z-index: 1;
-      }
-      .dropdown-content a {
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-      }
-      .dropdown a:hover {background-color: #ddd;}
-      .show {display: block;}
-    </style>
-
     <title>spectrum analyzer</title>
     </head>
     <body>
+
       <h1>Spectrum Analyzer</h1>
       <style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}
         .button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;
         text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-      </style></head>
+      </style>
       
       <form action="/get">
         Brightness: <input type="text" name="input_brightness">
